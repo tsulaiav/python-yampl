50940b1f7293097c4fef6736849eaaf7847cb112
Vakho Tsulaia <vakhtang.tsulaia@cern.ch>
2017-03-09 17:12:05 -0800

    Added yampl_include_2 to setup.py to work around build issues

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from Cython.Distutils import build_ext
 
 yampl_include = subprocess.check_output(["pkg-config", "yampl", "--cflags"]).decode("utf-8")[2:-2]
 yampl_libs = subprocess.check_output(["pkg-config", "yampl", "--libs"]).decode("utf-8")[:-2]
-
+yampl_include_2 = yampl_include.replace("/include/yampl","/include")
 setup(
   name = "yampl",
   version = "1.0",
@@ -13,7 +13,7 @@ setup(
     Extension("yampl", 
               sources=["yampl.pyx"],
               libraries=["yampl"],
-              include_dirs=[yampl_include],
+              include_dirs=[yampl_include,yampl_include_2],
               extra_link_args=[yampl_libs],
               language="c++"),
     ],
